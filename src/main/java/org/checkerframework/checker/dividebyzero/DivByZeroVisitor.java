b17e20fc2ee546e6487f3cd2ae68ad63f1832db5
Navkiran <100175211+Navkirankn@users.noreply.github.com>
2023-10-25 21:11:31 -0700

    Update DivByZeroVisitor.java

--- src/main/java/org/checkerframework/checker/dividebyzero/DivByZeroVisitor.java
+++ src/main/java/org/checkerframework/checker/dividebyzero/DivByZeroVisitor.java
@@ -30,6 +30,11 @@ public class DivByZeroVisitor extends BaseTypeVisitor<DivByZeroAnnotatedTypeFact
     private boolean errorAt(BinaryTree node) {
         // A BinaryTree can represent any binary operator, including + or -.
         // TODO
+         if(DIVISION_OPERATORS.contains(node.getKind())) {
+            if(hasAnnotation(node.getRightOperand(), Zero.class) || hasAnnotation(node.getRightOperand(), Top.class)) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -43,6 +48,9 @@ public class DivByZeroVisitor extends BaseTypeVisitor<DivByZeroAnnotatedTypeFact
         // A CompoundAssignmentTree represents any binary operator combined with an assignment,
         // such as "x += 10".
         // TODO
+        if(DIVISION_OPERATORS.contains(node.getKind())) {
+            return hasAnnotation(node.getVariable(), Zero.class) || hasAnnotation(node.getVariable(), Top.class);
+        }
         return false;
     }
 
